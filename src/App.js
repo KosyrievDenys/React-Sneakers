9a3be2df018f688041f678167236e1f560029fc5
denys_kosyriev <utross2241@gmail.com>
2022-01-27 03:25:28 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import Card from './components/Card'
+import Card from './components/Card/Card'
 import Drawer from './components/Drawer';
 import Header from './components/Header';
+
+const arr = [
+	{
+		title: 'Мужские кросовки Nike Blazer Mid Suede',
+		price: 12999,
+		imageUrl: '/img/sneakers/1.jpg'
+	},
+	{
+		title: 'Мужские Кроссовки Nike Air Max 270',
+		price: 15600, 
+		imageUrl: '/img/sneakers/2.jpg'
+	},
+	{
+		title: 'Nike',
+		price: 8999, 
+		imageUrl: '/img/sneakers/3.jpg'
+	},
+	{
+		title: 'Puma',
+		price: 6000, 
+		imageUrl: '/img/sneakers/4.jpg'
+	},
+]
 
 function App() {
 	return (
@@ -17,8 +40,13 @@ function App() {
 				</div>
 
 				<div className="d-flex">
-					<Card />
-
+					{arr.map((obj) => (
+						<Card 
+						title={obj.title}
+						price={obj.price}
+						imageUrl={obj.imageUrl}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
